d34eb292e4c6d5be27ce84e5d998c449a2decc52
1chor <37573242+1chor@users.noreply.github.com>
2018-05-12 19:01:17 +0200

    Added comment and structure
    Added comment and structure

--- Memory_tb.vhd
+++ Memory_tb.vhd
@@ -25,7 +25,7 @@ architecture beh of memory_tb is
 
 	constant CLK_PERIOD			: time := 5 ns;
 	
-	constant ENABLE_16_BIT		: integer := 1; -- 8 bit
+	constant ENABLE_16_BIT		: integer := 1; -- 16 bit
 	constant FIFO_DEPTH_WRITE	: integer := 8;
 	constant FIFO_DEPTH_READ  	: integer := 8;
 	
@@ -80,227 +80,121 @@ begin
         rst <= '0';
 		wait for 100 ns;
 		
-		--RAM test
-		
---		r_w <= '1';
---		address <= "000000000000000000000000001";
---		if ENABLE_16_BIT = 1 then
---		  data_in <= "0000000100000001";
---		else
---		  --data_in <= "00000001";
---		end if;
---		wait for 1000 ns;
-		
---        address <= "000000000000000000000000010";
---		if ENABLE_16_BIT = 1 then
---          data_in <= "0000001000000010";
---        else
---          --data_in <= "00000010";
---        end if;        
---        wait for 1000 ns;
-        
---        address <= "000000000000000000000000011";
---        if ENABLE_16_BIT = 1 then
---          data_in <= "0000001100000011";
---        else
---          --data_in <= "00000011";
---        end if;
---        wait for 1000 ns;
-
---		r_w <= '0';
---		address <= "000000000000000000000000001";
---		wait for 1000 ns;
---		if ENABLE_16_BIT = 1 then
---          assert data_out = "0000000100000001" report "Valid data output" severity error;
---        else
---          assert data_out = "00000001" report "Valid data output" severity error;
---        end if;
-		
---		address <= "000000000000000000000000010";
---        wait for 1000 ns;
---        if ENABLE_16_BIT = 1 then
---          assert data_out = "0000001000000010" report "Valid data output" severity error;
---        else
---          assert data_out = "00000010" report "Valid data output" severity error;
---        end if;
-        
---        address <= "000000000000000000000000011";
---        wait for 1000 ns;
---        if ENABLE_16_BIT = 1 then
---          assert data_out = "0000001100000011" report "Valid data output" severity error;
---        else
---          assert data_out = "00000011" report "Valid data output" severity error;
---        end if;
-        
---        wait for 1000 ns;
-        
-        
         --Buffer test
-        r_w <= '1';
+		
+        r_w <= '1'; -- write
+		
         address <= "000000000000000000000000100";
-        if ENABLE_16_BIT = 1 then
-          data_in <= "0000010000000100";
-        else
-          --data_in <= "00000001";
-        end if;
+        data_in <= "0000010000000100";
+        
         wait for 10 ns;
         
         address <= "000000000000000000000000101";
-        if ENABLE_16_BIT = 1 then
-          data_in <= "0000101000000101";
-        else
-          --data_in <= "00000010";
-        end if;        
-        wait for 10 ns;
+        data_in <= "0000101000000101";
+        
+		wait for 10 ns;
         
         address <= "000000000000000000000000110";
-        if ENABLE_16_BIT = 1 then
-          data_in <= "0000011000000110";
-        else
-          --data_in <= "00000011";
-        end if;
-        wait for 10 ns;
+        data_in <= "0000011000000110";
+        
+		wait for 10 ns;
         
         address <= "000000000000000000000000111";
-        if ENABLE_16_BIT = 1 then
-          data_in <= "0000011100000111";
-        else
-          --data_in <= "00000001";
-        end if;
-        wait for 10 ns;
+        data_in <= "0000011100000111";
+        
+		wait for 10 ns;
         
         address <= "000000000000000000000001000";
-        if ENABLE_16_BIT = 1 then
-          data_in <= "0000101000001000";
-        else
-          --data_in <= "00000010";
-        end if;        
-        wait for 10 ns;
+        data_in <= "0000101000001000";
+        
+		wait for 10 ns;
         
         address <= "000000000000000000000001001";
-        if ENABLE_16_BIT = 1 then
-          data_in <= "0000011000001001";
-        else
-          --data_in <= "00000011";
-        end if;
-        wait for 10 ns;
+        data_in <= "0000011000001001";
+        
+		wait for 10 ns;
         
         address <= "000000000000000000000001010";
-        if ENABLE_16_BIT = 1 then
-          data_in <= "0000010000001010";
-        else
-          --data_in <= "00000001";
-        end if;
-        wait for 10 ns;
+        data_in <= "0000010000001010";
+        
+		wait for 10 ns;
         
         address <= "000000000000000000000001011";
-        if ENABLE_16_BIT = 1 then
-          data_in <= "0000101000001011";
-        else
-          --data_in <= "00000010";
-        end if;        
-        wait for 10 ns;
+        data_in <= "0000101000001011";
+        
+		wait for 10 ns;
 		  
-		  assert mem_ready = '0' report "Buffer overflow" severity error;
+		assert mem_ready = '0' report "Buffer overflow" severity error;
 		  
-		  while mem_ready = '0' loop
-			 wait for 10 ns;
-		  end loop;
+		while mem_ready = '0' loop
+			wait for 10 ns; -- wait until FIFO is not full
+		end loop;
 		  
-		  wait for 10 ns;
+		wait for 10 ns;
 		  
         address <= "000000000000000000000001100";
-        if ENABLE_16_BIT = 1 then
-          data_in <= "0000011000001100";
-        else
-          --data_in <= "00000011";
-        end if;
-        wait for 10 ns;
+        data_in <= "0000011000001100";
+        
+		wait for 10 ns;
 		  
         assert mem_ready = '0' report "Buffer overflow" severity error;
         
-         wait for 10000 ns;
+        wait for 10000 ns;
 			
 		------------------------------------------------------------------
-        
-        r_w <= '0';
+		
+		r_w <= '0'; -- read
+		
         address <= "000000000000000000000000100";
-        if ENABLE_16_BIT = 1 then
-          assert data_out = "0000010000000100" report "Valid data output" severity error;
-        else
-          --data_in <= "00000001";
-        end if;
-        wait for 10 ns;
+        assert data_out = "0000010000000100" report "Valid data output" severity error;
+        
+		wait for 10 ns;
         
         address <= "000000000000000000000000101";
-        if ENABLE_16_BIT = 1 then
-          assert data_out = "0000101000000101" report "Valid data output" severity error;
-        else
-          --data_in <= "00000010";
-        end if;        
-        wait for 10 ns;
+        assert data_out = "0000101000000101" report "Valid data output" severity error;
+        
+		wait for 10 ns;
         
         address <= "000000000000000000000000110";
-        if ENABLE_16_BIT = 1 then
-          assert data_out = "0000011000000110" report "Valid data output" severity error;
-        else
-          --data_in <= "00000011";
-        end if;
-        wait for 10 ns;
+        assert data_out = "0000011000000110" report "Valid data output" severity error;
+        
+		wait for 10 ns;
         
         address <= "000000000000000000000000111";
-        if ENABLE_16_BIT = 1 then
-          assert data_out = "0000011100000111" report "Valid data output" severity error;
-        else
-          --data_in <= "00000001";
-        end if;
-        wait for 10 ns;
+        assert data_out = "0000011100000111" report "Valid data output" severity error;
+        
+		wait for 10 ns;
         
         address <= "000000000000000000000001000";
-        if ENABLE_16_BIT = 1 then
-          assert data_out = "0000101000001000" report "Valid data output" severity error;
-        else
-          --data_in <= "00000010";
-        end if;        
-        wait for 10 ns;
+        assert data_out = "0000101000001000" report "Valid data output" severity error;
+        
+		wait for 10 ns;
         
         address <= "000000000000000000000001001";
-        if ENABLE_16_BIT = 1 then
-          assert data_out = "0000011000001001" report "Valid data output" severity error;
-        else
-          --data_in <= "00000011";
-        end if;
-        wait for 10 ns;
+        assert data_out = "0000011000001001" report "Valid data output" severity error;
+        
+		wait for 10 ns;
         
         address <= "000000000000000000000001010";
-        if ENABLE_16_BIT = 1 then
-          assert data_out = "0000010000001010" report "Valid data output" severity error;
-        else
-          --data_in <= "00000001";
-        end if;
-        wait for 10 ns;
+        assert data_out = "0000010000001010" report "Valid data output" severity error;
+        
+		wait for 10 ns;
         
         address <= "000000000000000000000001011";
-        if ENABLE_16_BIT = 1 then
-          assert data_out = "0000101000001011" report "Valid data output" severity error;
-        else
-          --data_in <= "00000010";
-        end if;        
-        wait for 10 ns;
+        assert data_out = "0000101000001011" report "Valid data output" severity error;
+        
+		wait for 10 ns;
 		  
-		  assert mem_ready = '0' report "Buffer overflow" severity error;
+		assert mem_ready = '0' report "Buffer overflow" severity error;
 		  
-		  while mem_ready = '0' loop
-			 wait for 100 ns;
-		  end loop;
+		while mem_ready = '0' loop
+			wait for 100 ns; -- wait until FIFO is not full
+		end loop;
         
         address <= "000000000000000000000001100";
-        if ENABLE_16_BIT = 1 then
-          assert data_out = "0000011000001100" report "Valid data output" severity error;
-        else
-          --data_in <= "00000011";
-        end if;
-        wait for 10 ns;
+        assert data_out = "0000011000001100" report "Valid data output" severity error;
+        
+		wait for 10 ns;
         
         assert mem_ready = '0' report "Buffer overflow" severity error;
         
